c14362a1ad43a71588c464107ef7cd5a54b61e99
PandaAcademy <kontakt@pandait.pl>
2022-05-29 09:55:55 +0200

    Update backend.groovy

--- jenkins_controller/jobs/backend.groovy
+++ jenkins_controller/jobs/backend.groovy
@@ -5,9 +5,9 @@ pipelineJob('backend') {
                 git {
                     remote {
                         credentials("")
-                        url("https://github.com/PandaAcademy/Backend.git")
+                        url("https://github.com/Panda-Academy-Core-2-0/Backend.git")
                     }
-                    branches('master')
+                    branches('main')
                     scriptPath('Jenkinsfile')
                 }
             }
